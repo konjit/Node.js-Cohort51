dfb7fe71d0399bdde442141de574a187a2d095a3
konjit <kkll6349@gmail.com>
2025-02-21 21:56:58 +0100

    Added handler for each file operation.

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,10 @@
-const express = require("express");
+import express from "express";
+import path from "path";
+import fs from "fs";
+
 const app = express();
-const path = require("path");
 
-const fs = require("fs");
-
-const blogsDir = path.join(__dirname, "blogs");
+const blogsDir = path.resolve("blogs");
 
 app.use(express.json());
 
@@ -16,10 +16,16 @@ app.get("/", function (req, res) {
 app.post("/blogs", (req, res) => {
   const { title, content } = req.body;
 
+  if (!title || !content) {
+    return res.status(400).send({ message: "Content is required." });
+  }
   const filePath = path.join(blogsDir, title);
+  const submitBlog = () => {
+    fs.writeFileSync(filePath, content);
+    res.status(200).send({ message: "Blog is created successfully." });
+  };
 
-  fs.writeFileSync(filePath, content);
-  res.status(201).send({ message: "Ok" });
+  handleFileOperation(submitBlog, res, filePath, content);
 });
 
 // Update a blog
@@ -32,26 +38,29 @@ app.put("/posts/:title", (req, res) => {
   }
 
   const filePath = path.join(blogsDir, title);
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).send({ message: `Post ${title} not found.` });
-  }
+  checkFileExists(filePath, res);
 
-  fs.writeFileSync(filePath, content);
-  res.status(200).send({ message: "Ok" });
+  const updateBlog = () => {
+    fs.writeFileSync(filePath, content);
+    res.status(201).send({ message: "Blog is updated successfully" });
+  };
+
+  handleFileOperation(updateBlog, res, filePath, content);
 });
 
 // Delete a blog
 app.delete("/blogs/:title", (req, res) => {
   const title = req.params.title;
-  console.log(title);
+
   const filePath = path.join(blogsDir, title);
 
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).send({ message: `Post ${title} not found.` });
-  }
+  checkFileExists(filePath, res);
 
-  fs.unlinkSync(filePath);
-  res.status(200).send({ message: "Ok" });
+  const deleteBlog = () => {
+    fs.unlinkSync(filePath);
+    res.status(200).send({ message: "Blog is deleted successfully." });
+  };
+  handleFileOperation(deleteBlog, res, filePath);
 });
 
 // Read a blog
@@ -59,23 +68,47 @@ app.get("/blogs/:title", (req, res) => {
   const title = req.params.title;
   const filePath = path.join(blogsDir, title);
 
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).send("This post does not exist.");
-  }
+  checkFileExists(filePath, res);
 
-  const post = fs.readFileSync(filePath, "utf-8");
-  res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(post);
+  const encoding = "utf-8";
+  const readBlog = () => {
+    const post = fs.readFileSync(filePath, encoding);
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send(post);
+  };
+
+  handleFileOperation(readBlog, res, filePath, encoding);
 });
 
 // BONUS: Get all blogs
 app.get("/blogs/", (req, res) => {
   const blogs = fs.readdirSync(blogsDir);
-  if (!blogs) {
-    res.status(500).send({ message: " The resource posts does not exist." });
-  }
-  const blogTitles = blogs.map((blog) => ({ title: blog }));
-  res.status(200).send(blogTitles);
+  const getBlogs = () => {
+    if (!blogs) {
+      res.status(500).send({ message: "No blog is found." });
+    }
+    const blogTitles = blogs.map((blog) => ({ title: blog }));
+    res.status(200).send(blogTitles);
+  };
+
+  handleFileOperation(getBlogs, res, blogs);
 });
+
+/** A helper function that checks if a file operation is a success or failure. */
+
+const handleFileOperation = (operation, ...args) => {
+  try {
+    return operation(...args);
+  } catch (err) {
+    res.status(500).send({ message: `Error occurred while ${msg}` });
+  }
+};
+
+/*Checks if a specified file exists or not. */
+const checkFileExists = (filePath, res) => {
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).send("This blog does not exist.");
+  }
+};
 
 app.listen(3000);
